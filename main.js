766c03d581c20ea81407a541d4c7541cbfd4f013
Snicks08 <80923983+Snicks08@users.noreply.github.com>
2022-06-20 16:48:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var song1 = ""
+var song2 = ""
+
+function preload() {
+    song1 = loadSound("8d82b5_Star_Wars_The_Imperial_March_Theme_Song (1).mp3");
+    song2 = loadSound("8d82b5_Star_Wars_Cantina_Theme_Song.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play() {
+    song1.play();
+}
